3f5b0d2f4d5802fea192539dd43efb1a27a9a7a9
jvcarpena <jvcarpena2@gmail.com>
2025-01-10 16:34:56 +0800

    Completed the ui and create the simple route for home, register, and login

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Load .env file
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] == os.getenv("FLASK_KEY")
+Bootstrap5(app)
+
+# For temporary profile of the user
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
